6100ccf064ffd08f97ad7dbb017d2cffe72bc4e4
JuliusRose <orfa.production@gmail.com>
2022-03-19 12:42:10 +0100

    yes

---
+++ Slot/Slot.js
@@ -0,0 +1,74 @@
+let Money = 100;
+let TimesWon = 0;
+let TimesLost = 0; 
+
+let MoneyText = document.querySelector(".Money");
+let TimesWonText = document.querySelector(".TimesWon")
+let TimesLostText = document.querySelector(".TimesLost")
+
+
+let images = [
+"SlotImages/Apple.png",
+"SlotImages/Bar.png",
+"SlotImages/Cherry.png",
+"SlotImages/Coin.png",
+"SlotImages/Orange.png",
+"SlotImages/Pear.png",
+];
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach((die) => {
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+        let dieThreeValue = Math.floor(Math.random()*6);
+        document.querySelector("#Slot01").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#Slot02").setAttribute("src", images[dieTwoValue]);
+        document.querySelector("#Slot03").setAttribute("src", images[dieThreeValue]);
+        
+          if((dieOneValue + 1) == (dieTwoValue + 1) == (dieThreeValue + 1)) {
+              document.querySelector("#SlotTotal").innerHTML = "Win";
+              Money *= 1000;
+              TimesWon += 1;
+              MoneyText.innerHTML = `Money: ${Money} $`; 
+              TimesWonText.innerHTML = `TimesWon: ${TimesWon}`
+          }
+          else if((dieOneValue + 1) == (dieTwoValue + 1)) {
+            document.querySelector("#SlotTotal").innerHTML = "Semi-Win";
+            Money *= 10;
+            TimesWon += 1;
+            MoneyText.innerHTML = `Money: ${Money} $`; 
+            TimesWonText.innerHTML = `TimesWon: ${TimesWon}`
+        }
+        else if((dieTwoValue + 1) == (dieThreeValue + 1)) {
+            document.querySelector("#SlotTotal").innerHTML = "Semi-Win";
+            Money *= 10;
+            TimesWon += 1;
+            MoneyText.innerHTML = `Money: ${Money} $`; 
+            TimesWonText.innerHTML = `TimesWon: ${TimesWon}`
+        }
+        else if((dieOneValue + 1) == (dieThreeValue + 1)) {
+            document.querySelector("#SlotTotal").innerHTML = "Semi-Win";
+            Money *= 10;
+            TimesWon += 1;
+            MoneyText.innerHTML = `Money: ${Money} $`; 
+            TimesWonText.innerHTML = `TimesWon: ${TimesWon}`
+        }
+          else {
+              document.querySelector("#SlotTotal").innerHTML = "Lose";
+              Money *= 0.1;
+              TimesLost += 1;
+              MoneyText.innerHTML = `Money: ${Money} $`; 
+              TimesLostText.innerHTML = `TimesLost: ${TimesLost}` 
+          }
+    },
+    1500
+    );
+}
+roll();
