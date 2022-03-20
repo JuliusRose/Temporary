e3b9d59f8c9ba604663c720f5faf08df871017db
JuliusRose <orfa.production@gmail.com>
2022-03-20 11:38:05 +0100

    yes yes yes

---
+++ Roulette/Roulette.js
@@ -0,0 +1,39 @@
+let Money = 100;
+let TimesWon = 0;
+let TimesLost = 0; 
+
+let MoneyText = document.querySelector(".Money");
+let TimesWonText = document.querySelector(".TimesWon");
+let TimesLostText = document.querySelector(".TimesLost");
+
+let images = ["RouletteImages/wheel.png"];
+let wheel = document.querySelectorAll("img");
+
+function spin(){
+    wheel.forEach((die) => {
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        wheel.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let spinOneValue = Math.floor(Math.random()*37);
+        document.querySelector("#WheelTotal").innerHTML = "Your roll is " + (spinOneValue);
+
+        if(spinOneValue%2 == 0) {
+            Money *= 2;
+            TimesWon += 1;
+            MoneyText.innerHTML = `Money: ${Money} $`; 
+            TimesWonText.innerHTML = `TimesWon: ${TimesWon}`
+        }
+        else {
+            Money *= 0.5;
+            TimesLost += 1;
+            MoneyText.innerHTML = `Money: ${Money} $`; 
+            TimesLostText.innerHTML = `TimesLost: ${TimesLost}`
+        }
+    },
+    3000
+    );
+}
+spin();
